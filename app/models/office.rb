32b4c32668eb77d7c65a13473ed1179af4cf285c
unknown <tvo22@uh.edu>
2019-10-29 01:19:17 -0500

    finish

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,2 +1,3 @@
 class Office < ApplicationRecord
+    belongs_to :physician
 end
--- app/models/physician.rb
+++ app/models/physician.rb
@@ -1,2 +1,9 @@
 class Physician < ApplicationRecord
+    # has_many :appointments, inverse_of: :physician
+    # accepts_nested_attributes_for :appointments, reject_if: :all_blank, allow_destroy: true
+
+    has_many :offices, inverse_of: :physician
+    accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy: true
+
+    has_many :appointments
 end
